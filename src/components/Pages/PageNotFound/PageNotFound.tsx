7a7047e6d9f03672cf4e1b4c39080738b1801aed
Jesse Lind <jesseclind@gmail.com>
2025-08-08 19:17:58 -0400

    feat(404 Page): update styles for 404 page

--- src/components/Pages/PageNotFound/PageNotFound.tsx
+++ src/components/Pages/PageNotFound/PageNotFound.tsx
@@ -5,18 +5,9 @@ import { ButtonLink } from '../../Common/ButtonLink'
 const PageNotFound = () => {
   return (
     <div className={styles.page_not_found}>
-      <div className='page__inner'>
-        <div className={styles.container}>
-          <div className={styles.image}>
-            <img src='/images/page_not_found.svg' alt='Page Not Found' />
-          </div>
-
-          <div className={styles.content}>
-            <h1>Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
-            <ButtonLink link='/' text='&larr; Back Home' size='large' />
-          </div>
-        </div>
+      <div className={styles.content}>
+        <h1>Page Not Found</h1>
+        <a href='../'>Go Back</a>
       </div>
     </div>
   )
