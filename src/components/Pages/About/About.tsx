b79fd47fb2b3271b258361442aec4bb80a063ee3
Jesse Lind <jesseclind@gmail.com>
2025-08-08 22:28:43 -0400

    feat(about): add about page file

---
+++ src/components/Pages/About/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './About.module.scss'
+
+const About = () => {
+  return (
+    <div className={styles.About}>
+      <div className={styles.content}></div>
+    </div>
+  )
+}
+
+export default About
