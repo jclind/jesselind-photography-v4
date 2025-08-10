ab43b857abed3ef153eb38c1aa86b6fa9af77267
Jesse Lind <jesseclind@gmail.com>
2025-08-09 20:07:42 -0400

    feat(about): add information and contact links to about page

--- src/components/Pages/About/About.tsx
+++ src/components/Pages/About/About.tsx
@@ -2,9 +2,57 @@ import React from 'react'
 import styles from './About.module.scss'
 
 const About = () => {
+  const contactLinks = [
+    {
+      label: 'Email',
+      value: 'jesselindphotography@gmail.com',
+      link: 'mailto:jesselindphotography@gmail.com',
+    },
+    {
+      label: 'Instagram',
+      value: '@jesselindphotography',
+      link: 'https://www.instagram.com/jesselindphotography/',
+    },
+    { label: 'Github', value: '@jclind', link: 'https://github.com/jclind' },
+    {
+      label: 'Website',
+      value: 'jesselind.com',
+      link: 'https://jesselind.com/',
+    },
+  ]
+
   return (
     <div className={styles.About}>
-      <div className={styles.content}></div>
+      <img src='/images/logo.webp' className={styles.logo} />
+      <div className={styles.content}>
+        <div className={styles.information}>
+          <h1>Information</h1>
+          <p>
+            I’m Jesse, a hobbyist photographer with 10+ years of shooting
+            experience, including two years of the Tookapic daily challenge. I
+            work across animals, landscapes, and urban scenes, and keep pushing
+            into new genres.
+          </p>
+          <p>
+            Design and visual style have always been part of my life, and I aim
+            to keep refining my eye. If you’d like to see more of my creations
+            check out my personal website.
+          </p>
+        </div>
+        <div className={styles.contact}>
+          <h1>Contact</h1>
+          <div className={styles.contact_links}>
+            {contactLinks.map(link => (
+              <div className={styles.contact_link}>
+                <span className={styles.label}>{link.label}</span>
+                <a href={link.link} target='_blank' rel='noopener noreferrer'>
+                  {link.value}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
